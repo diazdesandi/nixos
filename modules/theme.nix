8284086df76c3e2a4125c312f1853fedcec64193
rene <diaz.de.sandi.7@gmail.com>
2026-01-31 21:38:12 -0800

    Remove qt5ct and qt6ct from theme configuration

--- modules/theme.nix
+++ modules/theme.nix
@@ -5,8 +5,6 @@
 {
   environment.systemPackages = with pkgs; [
     nwg-look
-    qt5ct
-    qt6ct
 
     nerd-fonts.jetbrains-mono
     nerd-fonts.fira-code
@@ -14,8 +12,4 @@
     catppuccin-cursors.mochaDark
     papirus-icon-theme
   ];
-
-  environment.sessionVariables = {
-    QT_QPA_PLATFORMTHEME = "qt5ct";
-  };
 }
